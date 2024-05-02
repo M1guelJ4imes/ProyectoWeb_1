8f863da98a28b14cd9def3aa6ce0f0b96b48203f
Miguel <miguel2221895@correo.uis.edu.co>
2024-05-01 21:55:19 -0500

    Ajustes

--- proyectoweb1/libreria/views.py
+++ proyectoweb1/libreria/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+#Acceso a las paginas del sitio
 def inicio(request):
     return render(request, 'paginas/inicio.html')
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
+
+#Acceso a los libros
 def libros(request):
     return render(request, 'libros/index.html')
+def crear(request):
+    return render(request, 'libros/crear.html')
